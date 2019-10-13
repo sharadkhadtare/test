b4bb8653453bc346a4e34e151a058854eb51c8c8
sharadkhadtare <sharadkhadtare@yahoo.com>
2019-10-13 22:35:53 +0530

    modify jenkinsfile

--- main.tf
+++ main.tf
@@ -16,23 +16,29 @@ resource "random_id" "instance_id" {
  byte_length = 8
 }
 
-module "cloud-sql-gdpr-us" {
-# source = "git@github.com:procter-gamble/terraform-module-pg-cloud-sql?ref=v1.2.0"
-  source = "/home/sharad/test/tsgd"
+module "private_service_access" {
+  source  = "/home/sharad/test786/tgsd/modules/private_service_access"
   project_id = "spikey-prem"
-  mysql_name = "gdprsql"
-  mysql_version = "MYSQL_5_6"
+  vpc_network = "vpc-prem"
+}
+
+module "cloud-sql-gdpr-us" {
+  source  = "/home/sharad/test786/tgsd/modules/mysql"
+
+  project_id = "spikey-prem"
+  name = "gdprsql"
+  database_version = "MYSQL_5_6"
   region = "us-east4"
   zone = "a"
-  private_network = "vpc-prem"
+ 
   additional_users = [
   {
-  name = "GDPRSegAggregate"
+  name = "GDPRSegInAgr"
   password = "4sk2u2Ab7jH"
   },
   {
-  name = "GDPRSegGenerate"
-  } 
+  name = "GDPRSegInGen"
+  }
 
   ]
 }
